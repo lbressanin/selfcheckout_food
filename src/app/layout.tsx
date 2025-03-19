cdd3d32eef4aaed4b73d795e951a890c6cc00b86
Lucas Bressanin Galvão <lucasbressanin@hotmail.com>
2025-03-19 10:44:43 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,18 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
-const poppings = Poppins({
+import { Toaster } from "@/components/ui/sonner";
+
+import { CartProvider } from "./[slug]/menu/contexts/cart";
+
+const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "FSW Donalds",
-  description: "Fast Food",
+  description: "Bora finalizar esse projeto lindo!",
 };
 
 export default function RootLayout({
@@ -20,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppings.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <CartProvider>{children}</CartProvider>
+
+        <Toaster/>
+      </body>
     </html>
   );
 }
